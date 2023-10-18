323fa6d29cfb019fd32c59269ac0435bccf5c852
AXAX-Kuddy <chromatik13@gmail.com>
2023-10-18 15:25:33 +0900

    dd

--- js/greetings.js
+++ js/greetings.js
@@ -2,16 +2,18 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 const loginIcon = document.querySelector("#login-icon");
+const afterLogin = document.querySelector("#after-login");
 const greeting = document.querySelector("#greeting");
 const weather = document.querySelector("#weather");
 
 const HIDDEN_CLASSNAME = "hidden";
+const FLEX_CLASSNAME = "flex";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  loginIcon.classList.add(HIDDEN_CLASSNAME);
+  loginForm.className = HIDDEN_CLASSNAME;
+  loginIcon.className = HIDDEN_CLASSNAME;
   localStorage.setItem(USERNAME_KEY, loginInput.value);
   paintGreetings();
 }
@@ -19,15 +21,18 @@ loginForm.addEventListener("submit", onLoginSubmit);
 
 function paintGreetings() {
   const username = localStorage.getItem(USERNAME_KEY);
+  afterLogin.className = FLEX_CLASSNAME;
   greeting.innerText = `${username}. Wellcome!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
-  weather.classList.remove(HIDDEN_CLASSNAME);
+  weather.className = FLEX_CLASSNAME;
+  loginForm.className = HIDDEN_CLASSNAME;
+  loginIcon.className = HIDDEN_CLASSNAME;
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername === null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginIcon.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.className = FLEX_CLASSNAME;
+  loginIcon.className = FLEX_CLASSNAME;
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings();
